a3c6674a729a6acc950c9da140b63d96b6d0462d
Quantados <kevi.jones2018@gmail.com>
2021-07-30 16:12:10 -0700

    Add project files.

---
+++ Bot.cs
@@ -0,0 +1,64 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.EventArgs;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using normality.Commands;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace normality
+{
+    public class Bot
+    {
+        public DiscordClient Client { get; private set;  }
+        public CommandsNextExtension Commands {  get; private set; }
+       
+        public async Task RunAsync()
+        
+        {
+            var json = string.Empty;
+            using(var fs = File.OpenRead("config.json")) 
+                using (var sr = new StreamReader(fs, new UTF8Encoding(false)))
+                json = await sr.ReadToEndAsync().ConfigureAwait(false);
+
+            var configjson = JsonConvert.DeserializeObject<Configjson>(json);
+
+            var config = new DiscordConfiguration()
+            {
+                Token = configjson.Token,
+                TokenType = TokenType.Bot,
+                AutoReconnect = true,
+                MinimumLogLevel = LogLevel.Debug,
+            };
+            
+            Client = new DiscordClient(config);
+            Client.Ready += OnClientReady;
+            var commandsConfig = new CommandsNextConfiguration()
+            {
+                StringPrefixes = new string[] {configjson.Prefix},
+                EnableDms = false,
+                EnableMentionPrefix = true,
+                DmHelp = true,
+                UseDefaultCommandHandler = true,
+            };
+            
+            Commands = Client.UseCommandsNext(commandsConfig);
+
+            Commands.RegisterCommands<FunCommands>();
+
+            await Client.ConnectAsync();
+            await Task.Delay(-1);        
+            }
+    
+        private Task OnClientReady(object sender, ReadyEventArgs e)
+
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Commands/FunCommands.cs
@@ -0,0 +1,29 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace normality.Commands
+{
+    public class FunCommands : BaseCommandModule
+    {
+        [Command("ping")]
+        [Description("Returns pong")]
+        public async Task Ping(CommandContext ctx)
+        {
+            await ctx.Channel.SendMessageAsync("Pong").ConfigureAwait(false);
+        }
+
+        [Command("add")]
+        [Description("Adds two numbers together")]
+        public async Task Add(CommandContext ctx, int numberOne, int numberTwo)
+        {
+            await ctx.Channel
+            .SendMessageAsync((numberOne + numberTwo).ToString())
+            .ConfigureAwait(false);
+        }
+    }
+}
